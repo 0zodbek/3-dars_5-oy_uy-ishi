a73279aec781ac69ac96651c7683008ecbbad07b
0zodbek <ozpdbekov@gmail.com>
2024-06-21 10:44:36 +0500

    l

--- app.js
+++ app.js
@@ -76,22 +76,82 @@
 // console.log(a);
 
 // 6-masala
-let students = [
-    { name: 'Alice', age: 20, score: 80, status: 'active' },
-    { name: 'Bob', age: 22, score: 92, status: 'inactive' },
-    { name: 'Charlie', age: 23, score: 85, status: 'active' }
-  ];
-  for (const iterator of students) {
+// let students = [
+//     { name: 'Alice', age: 20, score: 80, status: 'active', index: 1 },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive', index: 2 },
+//     { name: 'Charlie', age: 23, score: 85, status: 'active', index: 3 }
+//   ];
+//   let res ;
+//   for (const iterator of students) {
+//     if( 85 < iterator.score ){
+//     res = iterator.index
+//     }
+//   }
+// console.log(res);
+
+//7-masala
+// let students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' }
+//   ];
+//   let b = [];
+// for (const iterator of students) {
+//     if(iterator.status == 'active'){
+//     b.push(iterator)
+//     }
+// }
+// console.log(b);
+//8-masala
+// let students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 17, score: 88, status: 'active' }
+//   ];
+//   let kichik18 = 0;
+//   let res ;
+// for (const iterator of students) {
+//     if(iterator.age > 18 ){
     
-  }
-
-
-
-
-
-
-
-
+//     }else{
+//     kichik18 += 1
+//     }
+// }
+// if(kichik18 > 0){
+// res = false
+// }else{
+// res = true
+// }
+// console.log(res);
+//9-masala
+// let students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Charlie', age: 17, score: 88, status: 'active' }
+//   ];
+// let res ;
+// let test = 0 ;
+// for (const iterator of students) {
+//     if(iterator.score > 90){
+//     test += 1
+//     };
+//     if(test > 0 ){
+//     res = true
+//     }
+// }
+// console.log(res);
+//10-masala
+// let students1 = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+// let students2 = [
+//     { name: 'Charlie', age: 23, score: 88, status: 'active' },
+//     { name: 'David', age: 21, score: 90, status: 'inactive' }
+//   ];
+// let result = [] ;
+// result = students1.concat(students2);
+// console.log(result);
 
 
 
