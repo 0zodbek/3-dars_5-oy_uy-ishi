dc4999d4d63ccd4eecc814903c317c2a200efc59
0zodbek <ozpdbekov@gmail.com>
2024-06-21 09:42:10 +0500

    add 1 masala

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
 //1-masala
+ //**O'quvchilar ro'yxati:**
+   // **Masala:** Berilgan o'quvchilar ro'yxatidagi barcha o'quvchilarning ism va yoshini qaytaruvchi funksiyani yozing.
+     const students = [
+       { name: 'Alice', age: 20, score: 85, status: 'active' },
+       { name: 'Bob', age: 22, score: 92, status: 'inactive', nationaliy: 'american'}
+     ];
+     let a = [ ];
+ for (const iterator of students) {
+    a.push({
+        name: iterator.name,
+        age: iterator.age
+        })
+ }
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ********* string metodlari **********
+//1-masala
+    // let a = 'hello world';
+    // let b = a.substring(0,5);
+    // console.log(b);
+//2-masala
+    // let a = 'Hello world';
+    // let b = '';
+    // for(i = 0 ; i <= a.length ; i++){
+    //  if ( a.[i] == ){
+    
+    // } 
+    // }
+//     // console.log(b);
+//     let a = "Hello, WORLD!";
+// let b = swapCase(a);
+// console.log(b); // Output: hELLO, world!
+//     function swapCase(a) {
+//         return a.split('').map(function(value) {
+//           return value === value.toUpperCase() ? value.toLowerCase() : value.toUpperCase();
+//         }).join('');
+//       }
+   
