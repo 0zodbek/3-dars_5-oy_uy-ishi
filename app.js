f7b416e72e61ab0469ceb3a65e5066d97df03d80
0zodbek <ozpdbekov@gmail.com>
2024-06-21 10:23:19 +0500

    1

--- app.js
+++ app.js
@@ -1,37 +1,89 @@
 //1-masala
  //**O'quvchilar ro'yxati:**
    // **Masala:** Berilgan o'quvchilar ro'yxatidagi barcha o'quvchilarning ism va yoshini qaytaruvchi funksiyani yozing.
-     const students = [
-       { name: 'Alice', age: 20, score: 85, status: 'active' },
-       { name: 'Bob', age: 22, score: 92, status: 'inactive', nationaliy: 'american'}
-     ];
-     let a = [ ];
- for (const iterator of students) {
-    a.push({
-        name: iterator.name,
-        age: iterator.age
-        })
- }
-console.log(a);
+//      const students = [
+//        { name: 'Alice', age: 20, score: 85, status: 'active' },
+//        { name: 'Bob', age: 22, score: 92, status: 'inactive'}
+//      ];
+//      let a = [ ];
+//  for (const iterator of students) {
+//     a.push({
+//         name: iterator.name,
+//         age: iterator.age
+//         })
+//  }
+// console.log(a);
+
+// 2-masala
 
 
+// const students = [
+//            { name: 'Alice', age: 20, score: 85, status: 'active' },
+//            { name: 'Bob', age: 22, score: 92, status: 'inactive'}
+//          ];
+// let a = [];
+// let max = 0 ;
+// for (const iterator of students) {
+//     if ( max > iterator.score){
+//     max = max
+//     }else{
+//         max = iterator.score
+//     }
+// }
+
+// console.log(max);
 
 
+// 3-masala
+// let  students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Andrew', age: 23, score: 88, status: 'active' }
+//   ];
+  
+//   function m3(students) {
+//     let  a = students.filter(student => student.name[0].toUpperCase() === 'A');
+//     return a;
+//   }
+  
+//   let  a = m3(students);
+//   console.log(a); 
 
+// 4-masala
+// let students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'Bob', age: 22, score: 92, status: 'inactive' }
+//   ];
+//   let sum = 0 ;
 
+// for (const iterator of students) {
+//     sum = sum + iterator.score
+// }
+// console.log( sum / 2);
 
+//5-masala
+// let students = [
+//     { name: 'Alice', age: 20, score: 85, status: 'active' },
+//     { name: 'John', age: 22, score: 92, status: 'inactive' },
+//     { name: 'Bob', age: 23, score: 88, status: 'active' }
+//   ];
+//   let a ;
+// for (const iterator of students) {
+//     if (iterator.name === "John"){
+//     a = iterator
+//     }
+// }
+// console.log(a);
 
-
-
-
-
-
-
-
-
-
-
-
+// 6-masala
+let students = [
+    { name: 'Alice', age: 20, score: 80, status: 'active' },
+    { name: 'Bob', age: 22, score: 92, status: 'inactive' },
+    { name: 'Charlie', age: 23, score: 85, status: 'active' }
+  ];
+  for (const iterator of students) {
+    
+  }
 
 
 
